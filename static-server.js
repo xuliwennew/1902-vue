cc187191df83827b86cb310558564adec7ddc030
xuliwennew <624026015@qq.com>
2019-06-15 17:58:36 +0800

    day6

--- static-server.js
+++ static-server.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const path = require("path")
 
-//instance express create express server apache 
-const app = express() 
+//instance express create express server apache
+const app = express()
 
 //在express开启静态文件处理
 app.use(express.static(__dirname))
 
-app.use("/",(req,res)=>{
-    res.send("hello server")
+
+app.use("/api",(req,res)=>{
+    res.json({code:200})
 })
 
 app.listen(3000,()=>{
